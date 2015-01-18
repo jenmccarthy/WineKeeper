75462c8b23674ab05ad0e02a4d0788da5dd22e53
Jennifer McCarthy <jennifernward@gmail.com>
2015-01-17 18:51:37 -0800

    validate presence of new wine attributes

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,5 +1,8 @@
 class Wine < ActiveRecord::Base
   validates :name, :presence => true
+  validates :region, :presence => true
+  validates :year, :presence => true
+  validates :type_id, :presence => true
 
   belongs_to :type
 end
--- spec/models/wine_spec.rb
+++ spec/models/wine_spec.rb
@@ -2,5 +2,10 @@ require 'rails_helper'
 
 describe Wine do
   it { should validate_presence_of :name }
+  it { should validate_presence_of :region }
+  it { should validate_presence_of :year }
+  it { should validate_presence_of :type_id }
+
+
   it { should belong_to :type }
 end
