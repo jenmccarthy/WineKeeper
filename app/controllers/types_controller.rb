65a769e9b3f5649f07b9a19b9c166de4189c4d5c
Jennifer McCarthy <jennifernward@gmail.com>
2015-01-23 11:24:21 -0800

    allow all RESTful actions on a type object

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -16,9 +16,32 @@ class TypesController < ApplicationController
     end
   end
 
+  def edit
+    @type = Type.find(params[:id])
+  end
+
+  def update
+    @type = Type.find(params[:id])
+    if @type.update_attributes(type_params)
+      redirect_to type_path(@type)
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @type = Wine.find(params[:id])
+  end
+
+  def delete
+    @type = Type.find(params[:id])
+    @type.destroy
+  end
+
   private
 
   def type_params
     params.require(:type).permit(:name)
+  end
 
 end
